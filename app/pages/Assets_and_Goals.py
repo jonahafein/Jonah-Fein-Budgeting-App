d3b2948ec7cfef606a643db170af3c5a86e39bf9
Jonah Fein <jonahafein@Jonahs-MacBook-Air-4.local>
2026-04-17 00:10:55 -0400

    Data is now persistent - need to test update

--- app/pages/Assets_and_Goals.py
+++ app/pages/Assets_and_Goals.py
@@ -17,7 +17,12 @@ if not st.session_state.get("email"):
 
 # getting their user_id  
 db = Database()
-user_id = db.get_user(st.session_state.email)["user_id"]
+user = db.get_user(st.session_state.email)
+if not user:
+    st.error("User not found")
+    st.stop()
+
+user_id = user["user_id"]
 
 # setting the session state by pulling existing data from azure for this specific user_id
 if "loaded" not in st.session_state:
@@ -35,7 +40,7 @@ if "loaded" not in st.session_state:
     st.session_state.retirement = assets["retirement"] if assets else 0
     st.session_state.retirement_returns = assets["retirement_returns"] if assets else 0
     st.session_state.goals = goals if goals else []
-    st.session_state.home_data = home_data if home_data else []
+    st.session_state.home_data = home_data if home_data else None
     if debt_df:
         st.session_state.debt_df = pd.DataFrame([{
             "Item": d["debt_item"],
@@ -47,7 +52,7 @@ if "loaded" not in st.session_state:
     st.session_state.loaded = True
     
 
-st.title("Account Info")
+st.title("Assets and Goals")
 
 # initialize debt_df if not there
 if "debt_df" not in st.session_state:
@@ -75,25 +80,31 @@ if st.session_state.email:
     retirement_returns = st.number_input("Retirement expected returns:", value = st.session_state.retirement_returns)
     st.session_state.retirement_returns = retirement_returns
 
-
-    home = st.selectbox("Do you own a home?", ["no", "yes"])
+    home_exists = True if home_data else False
+    home = st.selectbox("Do you own a home?", ["no", "yes"], index=1 if home_exists else 0)
+    if home == "yes":
+        home_exists = True
+    else:
+        home_exists = False
+        
     
     # do an option for those who have paid off their house
     
-    if home == "yes":
-        paid = st.selectbox("Is it paid off?", ['no', 'yes'])
+    if home_exists:
+        paid_default = 1 if (home_data and home_data.get("paid_off")) else 0
+        paid = st.selectbox("Is it paid off?", ['no', 'yes'], index=paid_default)
         if paid == 'no': 
             col1,col2,col3, col4, col5 = st.columns(5)
             with col1:
-                years = st.number_input("How many years left on the mortgage?", key = "years")
+                years = st.number_input("How many years left on the mortgage?", value = st.session_state.home_data["years"] if st.session_state.home_data else 0)
             with col2:
-                balance = st.number_input("What is the remaining balance?", key = "debt_balance")
+                balance = st.number_input("What is the remaining balance?", value = st.session_state.home_data["balance"] if st.session_state.home_data else 0)
             with col3:
-                interest = st.number_input("What is the interest rate? (If variable, give an average)", key = "debt_interest")
+                interest = st.number_input("What is the interest rate? (If variable, give an average)", st.session_state.home_data["interest"] if st.session_state.home_data else 0)
             with col4:
-                fees = st.number_input("Typical monthly non-mortgage house costs?", key = "fees")  
+                fees = st.number_input("Typical monthly non-mortgage house costs?", st.session_state.home_data["fees"] if st.session_state.home_data else 0)  
             with col5:
-                value = st.number_input("What is your home's estimated value?", key = "value")             
+                value = st.number_input("What is your home's estimated value?", st.session_state.home_data["home_value"] if st.session_state.home_data else 0)             
             house_info = [years, balance, interest, fees, value]
         else:
             col1,col2, col3, col4, col5 = st.columns(5)
@@ -104,10 +115,17 @@ if st.session_state.email:
             with col3:
                 interest = None
             with col4:
-                value = st.number_input("What is your home's value?", key = "value")
+                value = st.number_input("What is your home's value?", st.session_state.home_data["home_value"]if st.session_state.home_data else 0)
             with col5:
-                fees = st.number_input("Typical monthly non-mortgage house costs?", key = "fees")
+                fees = st.number_input("Typical monthly non-mortgage house costs?", st.session_state.home_data["fees"] if st.session_state.home_data else 0)
             house_info = [years, balance, interest, value, fees]
+    else:
+        paid = None 
+        value = None 
+        years = None
+        balance = None 
+        interest = None
+        fees = None
     # add other necessary questions
 
     goals = st.multiselect('Goals:', ['Build up my emergency fund', 'invest/save for non retirement', 'invest for retirement', 'other'], default = st.session_state.goals)
@@ -116,7 +134,7 @@ if st.session_state.email:
         monthly_other = st.number_input("Estimate how many dollars a month you will need to put away for your other category:", key = "monthly_other")
     
     
-    st.write("List all of your non mortgage debt 1 by 1:")
+    st.write("List all of your non-mortgage debts:")
     if 'debt_df' not in st.session_state:
         st.session_state.debt_df = pd.DataFrame(columns = ["Item", "Balance", "Interest Rate"])
     
@@ -147,21 +165,23 @@ if st.session_state.email:
             "retirement_returns": retirement_returns if retirement > 0 else None,
             "goals": goals,
             "monthly_other": monthly_other if "other" in goals else None,
-            "debt_df": st.session_state.debt_df if st.session_state.debt_df else None,
+            "debt_df": st.session_state.debt_df if not st.session_state.debt_df.empty else None,
             "home": home,
             "paid": paid,
             "house_info": house_info if paid == "yes" else None
         }
         # TODO: make the update methods
-        # db = Database()
-        # user_id = db.get_user(email)["user_id"]
-        # db.update_non_home_assets(user_id, savings, apy, brokerage, brokerage_returns, retirement, retirement_returns)
-        # db.update_home(user_id, paid, value, years, balance, interest, fees)
-        # for goal in goals:
-        #     db.update_goal(user_id, goal)
-        # for _, row in st.session_state.debt_df.iterrows():
-        #     db.update_debt(user_id, row["Item"], row["Balance"], row["Interest Rate"])
-        
+        db = Database()
+        user_id = db.get_user(email)["user_id"]
+        db.update_non_home_assets(user_id, savings, apy, brokerage, brokerage_returns, retirement, retirement_returns)
+        paid_bool = True if paid == "yes" else False
+        if home == "yes":
+            db.update_home(user_id, paid_bool, value, years, balance, interest, fees)
+        else:
+            db.delete_home(user_id)
+        goals = list(st.session_state.goals)
+        db.update_goals(user_id, goals)
+        db.update_debts(user_id, st.session_state.debt_df)
         st.success("profile saved!")
     
         
