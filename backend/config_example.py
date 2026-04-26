3ef429d9276a8f4620c59cd16791e9fb5e32423f
Jonah Fein <jonahafein@Jonahs-MacBook-Air-4.local>
2026-04-26 15:06:49 -0400

    switched to supabase for auto login

--- backend/config_example.py
+++ backend/config_example.py
@@ -1,2 +1,3 @@
-connection_string = "YOUR_CONNECTION_STRING_HERE"
-groq_api_key = "YOUR_KEY_STRING_HERE"
+groq_api_key = "GROQ_KEY_HERE"
+publishable_key_supabase = "SUPABASE_KEY_HERE"
+supabase_url = "SUPABASE_URL_HERE.co"
